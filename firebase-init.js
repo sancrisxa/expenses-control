8b4eb968fb8d50afbf321cd469902deb393b226f
Samuel Cristiano Silva Xavier <samuel.xavier@montreal.com.br>
2023-05-22 19:43:35 -0300

    Finalizacao do front end

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4583dY_NvHH9kqFkb3KlcuQQOWk9VO9w",
+    authDomain: "controle-de-gastos-ae653.firebaseapp.com",
+    projectId: "controle-de-gastos-ae653",
+    storageBucket: "controle-de-gastos-ae653.appspot.com",
+    messagingSenderId: "1066000388922",
+    appId: "1:1066000388922:web:3f6b236f2b9a2fc2093603"
+};
+firebase.initializeApp(firebaseConfig);
